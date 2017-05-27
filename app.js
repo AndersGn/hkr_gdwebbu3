286cd407b767cba062794d46dde66703539a4461
AndersGn <graneman@yahoo.se>
2017-05-27 17:16:18 +0200

    test dropdown button for genre

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/contact', contact);
-app.use('/products', products);
+//app.use('/contact', routes);
+//app.use('/products', routes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
